866e59f592c42b28c2e082e591058ba4504782f4
mohammadtaha02 <m.taha.coursera@gmail.com>
2024-07-02 17:48:11 +0300

    a

--- user/checkUser.php
+++ user/checkUser.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Content-Type: application/json");
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 $servername = "localhost";
 $username = "root";
